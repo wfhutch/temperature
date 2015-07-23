fa1e7c890c49bf23443e56f6eff5b70a55d84afd
Wayne Hutchinson <wfh2727@gmail.com>
2015-07-22 20:16:36 -0500

    added for loop to js and added css

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
   number should be converted to. Then a button that,
   when clicked, displays the converted.
 */
+
 var tempInput = document.getElementById("tempField");
 var newTemp = document.getElementById("answer");
 
@@ -22,44 +23,37 @@ function toFahrenheit (temp) {
   return Math.round(temp * 9 / 5 + 32);
 }
 
+function clearInput() {
+  return (tempInput.value = "");
+}
+
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (e) {
+  var optionChosen = document.getElementsByName("conversion_type");
+    for (i = 0; i < optionChosen.length; i++) {
 
-  if (document.getElementById("celsiusButton") === true); {
-    var fahtemp = toFahrenheit(tempInput.value);
-    newTemp.innerHTML = "Temperature in fahrenheit = " + fahtemp + "&deg";
-    }
-    if (document.getElementById("fahrenheitButton") === true); {
-      var celsiusTemp = toCelsius(tempInput.value);
-      newTemp.innerHTML = "Temperature in celsius = " + celsiusTemp + "&deg";
-    }
+      if (optionChosen[i].checked && optionChosen[i].value === 'celsius') {
+        var fahAnswer = toFahrenheit(tempInput.value);
+        newTemp.innerHTML = tempInput.value + "&deg" +" celsius converts to " + fahAnswer + "&deg" + " fahrenheit.";
+        clearInput();
+      }
+
+      if (optionChosen[i].checked && optionChosen[i].value === 'fahrenheit') {
+        var celAnswer = toCelsius(tempInput.value);
+        console.log(celAnswer);
+        newTemp.innerHTML = tempInput.value + "&deg" + " fahrenheit converts to " + celAnswer + "&deg" + " celsius.";
+        clearInput();
+      }  
+    }  
   }
-
-//THIS WORKS BUT HAVE TO REFRESH PAGE EVERY TIME.  NEED TO FIX SO THAT DOESN'T HAPPEN
-//TRY TO FIGURE OUT HOW TO USE EXISTING TEMPLATE.
-
-
-  // {
-  //   document.getElementById("answer").innerHTML = "Converts to " + toFahrenheit(tempInput) + " degrees Fahrenheit";
-  //   } else {
-  //   document.getElementById("answer").innerHTML = "Converts to " + toCelsius(tempInput) + " degrees Celsius";
-  //   }
-  // }
 
   // This will actually return an array containing both of the radio buttons
   // so you will need to look at each item in the array and see if the 'checked'
   // property is true (sample code provided)
-  // var optionChosen = document.getElementsByName("conversion_type");
-
-  // for loop
-    // if (?.checked && ?.value === 'celsius') {
-
-    //}
-  // end for loop
-
-
+  
 // Get a reference to the button element in the DOM
+
 var button = document.getElementById("converter");
 
 // Assign a function to be executed when the button is clicked
